3bcee0a9718fb5d278b23697e87657f99e0faf29
julchu <jchumtl@gmail.com>
2023-05-26 12:51:58 -0400

    Changing font size, removing accidental PDF

--- src/app/(page)/Banner.tsx
+++ src/app/(page)/Banner.tsx
@@ -10,8 +10,8 @@ const Banner: FC<{ title: string; subtitle: string }> = ({
         "relative top-[60%] flex h-[30%] flex-col place-content-center gap-y-3 bg-white/5 pl-[5cqw] leading-loose text-white backdrop-blur-sm backdrop-brightness-50"
       }
     >
-      <h1 className={"text-[7cqw]/[7cqw]"}>{title}</h1>
-      <h3 className={"text-[3cqw]/[3cqw]"}>{subtitle}</h3>
+      <h1 className={"text-[6.5cqw]/[6.5cqw]"}>{title}</h1>
+      <h3 className={"text-[2cqw]/[2cqw]"}>{subtitle}</h3>
     </div>
   );
 };
